666382fc5ac34de16143d1c0c36cb081a7148d75
frank <franktian424@qq.com>
2022-01-19 22:29:02 +0800

    update

--- d4rl/gym_mujoco/gym_envs.py
+++ d4rl/gym_mujoco/gym_envs.py
@@ -70,6 +70,14 @@ def get_noisy_hopper_env(**kwargs):
 def get_noisy_walker_env(**kwargs):
     return NormalizedBoxEnv(OfflineWalker2dEnv(DEFAULT_NOISE_PARAMS, **kwargs))
 
+class MixedOfflineHopperEnv(Walker2dEnv, offline_env.MixedOfflineEnv):
+    def __init__(self, noise_params=None, **kwargs):
+        Walker2dEnv.__init__(self, noise_params)
+        offline_env.MixedOfflineEnv.__init__(self, **kwargs)
+
+def get_mixed_hopper_env(**kwargs):
+    return NormalizedBoxEnv(MixedOfflineHopperEnv(**kwargs))
+
 
 if __name__ == '__main__':
     env = get_noisy_ant_env()
--- d4rl/offline_env.py
+++ d4rl/offline_env.py
@@ -130,6 +130,56 @@ class OfflineEnv(gym.Env):
         dataset_file.close()
         return data_dict
 
+class MixedOfflineEnv(gym.Env):
+    def __init__(self, dataset_urls=None, ref_max_score=None, ref_min_score=None, **kwargs):
+        super(MixedOfflineEnv, self).__init__(**kwargs)
+        self.dataset_urls = self._dataset_urls = dataset_urls
+        self.ref_max_score = ref_max_score
+        self.ref_min_score = ref_min_score
+
+    def get_normalized_score(self, score):
+        if (self.ref_max_score is None) or (self.ref_min_score is None):
+            raise ValueError("Reference score not provided for env")
+        return (score - self.ref_min_score) / (self.ref_max_score - self.ref_min_score)
+
+    @property
+    def dataset_filepath(self):
+        return filepath_from_url(self.dataset_url)
+
+    def get_dataset(self, h5path=None):
+        if h5path is None:
+            if self._dataset_url is None:
+                raise ValueError("Offline env not configured with a dataset URL.")
+            h5path = download_dataset_from_url(self.dataset_url)
+
+        data_dict = {}
+        with h5py.File(h5path, 'r') as dataset_file:
+            for k in tqdm(get_keys(dataset_file), desc="load datafile"):
+                try:  # first try loading as an array
+                    data_dict[k] = dataset_file[k][:]
+                except ValueError as e:  # try loading as a scalar
+                    data_dict[k] = dataset_file[k][()]
+
+        # Run a few quick sanity checks
+        for key in ['observations', 'actions', 'rewards', 'terminals']:
+            assert key in data_dict, 'Dataset is missing key %s' % key
+        N_samples = data_dict['observations'].shape[0]
+        if self.observation_space.shape is not None:
+            assert data_dict['observations'].shape[1:] == self.observation_space.shape, \
+                'Observation shape does not match env: %s vs %s' % (
+                    str(data_dict['observations'].shape[1:]), str(self.observation_space.shape))
+        assert data_dict['actions'].shape[1:] == self.action_space.shape, \
+            'Action shape does not match env: %s vs %s' % (
+                str(data_dict['actions'].shape[1:]), str(self.action_space.shape))
+        if data_dict['rewards'].shape == (N_samples, 1):
+            data_dict['rewards'] = data_dict['rewards'][:, 0]
+        assert data_dict['rewards'].shape == (N_samples,), 'Reward has wrong shape: %s' % (
+            str(data_dict['rewards'].shape))
+        if data_dict['terminals'].shape == (N_samples, 1):
+            data_dict['terminals'] = data_dict['terminals'][:, 0]
+        assert data_dict['terminals'].shape == (N_samples,), 'Terminals has wrong shape: %s' % (
+            str(data_dict['rewards'].shape))
+        return data_dict
 
 class OfflineEnvWrapper(gym.Wrapper, OfflineEnv):
     """
